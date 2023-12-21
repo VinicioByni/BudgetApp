ea6bb2b5ead8b946c36af3f9128e6c480b05288d
Vinicio Chávez <viniciochz@gmail.com>
2023-12-20 20:29:51 -0600

    Added clear table parameters button in expense table partial view

--- BudgetApp/wwwroot/Scripts/ExpenseTable/Models/TableParametersType.ts
+++ BudgetApp/wwwroot/Scripts/ExpenseTable/Models/TableParametersType.ts
@@ -1,5 +1,12 @@
-﻿export type TableParameters = {
+﻿
+// Move to more common folder later when working on Tiles
+export type periodInitialDate = {
     periodInitialDate?: string
+}
+
+
+
+export type TableParameters = periodInitialDate & {
     searchString?: string
     searchDate?: string
     sort?: string
